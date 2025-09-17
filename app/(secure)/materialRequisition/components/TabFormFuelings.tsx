b0956526d2b3b1e98f351d2ec442e153c93b1cbc
Gustavo-Ennes <gustavo.ennes@3778.care>
2025-09-17 15:45:55 -0300

    Mat. Req.: focus on date when add fueling

--- app/(secure)/materialRequisition/components/TabFormFuelings.tsx
+++ app/(secure)/materialRequisition/components/TabFormFuelings.tsx
@@ -1,5 +1,7 @@
+"use client";
+
 import { Grid, TextField, Button } from "@mui/material";
-import type { SetStateAction } from "react";
+import { type SetStateAction, useRef } from "react";
 import { FuelingFormList } from "./FuelingFormList";
 import type { FuelingData } from "../types";
 import { DatePicker } from "@mui/x-date-pickers";
@@ -25,54 +27,64 @@ export const TabFormFuelings = ({
   removeFueling: (idx: number) => void;
   addFueling: () => void;
   fuelings: FuelingData[];
-}) => (
-  <Grid container spacing={2} sx={{ height: "100%" }}>
-    <Grid size={4}>
-      <DatePicker
-        value={date}
-        onChange={(e) => (e ? setDate(startOfDay(e)) : undefined)}
-        sx={{ width: 1 }}
-        label="Data"
-        format="dd/MM/yyyy"
-        slotProps={{
-          textField: { size: "small" },
-        }}
-      />
-    </Grid>
-    <Grid size={3}>
-      <TextField
-        size="small"
-        fullWidth
-        type="number"
-        label="Qtd"
-        value={quantity}
-        onChange={(e) => setQuantity(Number(e.target.value))}
-      />
-    </Grid>
-    <Grid size={3}>
-      <TextField
-        size="small"
-        fullWidth
-        type="number"
-        label="Km/Hr."
-        value={kmHr ?? ""}
-        onChange={(e) =>
-          setKmHr(e.target.value ? Number(e.target.value) : undefined)
-        }
-      />
-    </Grid>
-    <Grid size={2} justifyContent={"center"} alignItems={"center"}>
-      <Button
-        variant="outlined"
-        onClick={addFueling}
-        disabled={!date || quantity <= 0}
-      >
-        +
-      </Button>
-    </Grid>
+}) => {
+  const dateInputRef = useRef<HTMLInputElement>(null);
 
-    <Grid size={12}>
-      <FuelingFormList fuelings={fuelings} onRemove={removeFueling} />
+  const handleAddFueling = () => {
+    addFueling();
+    dateInputRef?.current?.focus();
+  };
+
+  return (
+    <Grid container spacing={2} sx={{ height: "100%" }}>
+      <Grid size={4}>
+        <DatePicker
+          value={date}
+          onChange={(e) => (e ? setDate(startOfDay(e)) : undefined)}
+          sx={{ width: 1 }}
+          label="Data"
+          format="dd/MM/yyyy"
+          inputRef={dateInputRef}
+          slotProps={{
+            textField: { size: "small" },
+          }}
+        />
+      </Grid>
+      <Grid size={3}>
+        <TextField
+          size="small"
+          fullWidth
+          type="number"
+          label="Qtd"
+          value={quantity}
+          onChange={(e) => setQuantity(Number(e.target.value))}
+        />
+      </Grid>
+      <Grid size={3}>
+        <TextField
+          size="small"
+          fullWidth
+          type="number"
+          label="Km/Hr."
+          value={kmHr ?? ""}
+          onChange={(e) =>
+            setKmHr(e.target.value ? Number(e.target.value) : undefined)
+          }
+        />
+      </Grid>
+      <Grid size={2} justifyContent={"center"} alignItems={"center"}>
+        <Button
+          variant="outlined"
+          onClick={handleAddFueling}
+          disabled={!date || quantity <= 0}
+        >
+          +
+        </Button>
+      </Grid>
+
+      <Grid size={12}>
+        <FuelingFormList fuelings={fuelings} onRemove={removeFueling} />
+      </Grid>
     </Grid>
-  </Grid>
-);
+  );
+};
